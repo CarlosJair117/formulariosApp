3edc2decb09a7d58e6e1a1f5d0a57dd05181857c
CarlosJair117 <carlos.fdez.guar19@gmail.com>
2020-11-12 23:58:42 -0600

    formulario por template

---
+++ src/app/pages/template/template.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent implements OnInit {
+
+  usuario = {
+    nombre: 'carlos',
+    apellido: 'fernandez',
+    correo: 'carlos@gmail.com'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  guardar(forma: NgForm){
+    console.log(forma);
+    
+    if ( forma.invalid ) {
+      Object.values( forma.controls ).forEach( control => {
+        control.markAsTouched();
+      });
+      return
+    }
+
+    console.log(forma.value);
+
+  }
+
+}
